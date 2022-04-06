ed6908af01a2e9a2a33d2abadb332814b88d0fc9
LucasWu <1600475759@qq.com>
2022-04-06 09:19:25 -0700

    Fix Bug!

--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost:5432/shufa"
 
+# echo=True表示引擎将用repr()函数记录所有语句及其参数列表到日志
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, encoding='utf8', echo=False, poolclass=NullPool
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
